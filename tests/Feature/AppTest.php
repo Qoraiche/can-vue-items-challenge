94981e9aa1e3aa6fb564ef0df941bf4d39d8e878
Yassine <qoraicheofficiel@hotmail.com>
2019-06-13 18:34:32 +0100

    more testing

--- tests/Feature/AppTest.php
+++ tests/Feature/AppTest.php
@@ -38,11 +38,28 @@ class AppTest extends TestCase
 
         $response->assertStatus(200);
     }
-    /**
-     * Function name explain itself 
-     *
-     * @return void
-     */
+    
+    /** @test **/
+    public function user_can_add_item()
+    {
+
+        // we want to see the errros
+        // 
+        $this->withoutExceptionHandling();
+
+        // $user = factory(User::class)->create();
+
+        $this->actingAs(factory(User::class)->create());
+
+        // When they the endpoint /items to create a new item, while passing the necessary data
+
+        $this->post('/items', $this->defaulItemtAttr()->all());
+
+        // Then there should be a new in the database
+
+        $this->assertDatabaseHas('items', $this->defaulItemtAttr()->all());
+
+    }
 
     /** @test **/
     public function a_user_can_have_an_item() 
