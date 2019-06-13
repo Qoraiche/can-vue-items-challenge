4ea1b1069dbad5df03c8a09c6e308a338ba51dde
Yassine <qoraicheofficiel@hotmail.com>
2019-06-13 17:04:00 +0100

    add javascript vars to js package to add global user data vars which we will need later for vuejs

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use JavaScript;
 
 class AppController extends Controller
 {
     public function home()
     {
+        JavaScript::put([
+           'user_auth' => Auth::check(),
+           'user_data' => Auth::user(),
+        ]);
+
 		return view('welcome');
+
 	}
 }
